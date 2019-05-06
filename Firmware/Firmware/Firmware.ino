a739349f2b36ef632e85d92873f93fcd1e05996a
Markus Zinggl <markus.zinggl@outlook.com>
2019-05-06 15:09:31 +0200

    Added the solution for the firmware.

---
+++ Firmware/Firmware/Firmware.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Firmware.ino
+ Created:	06-May-19 3:00:09 PM
+ Author:	Markus Zinggl
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
